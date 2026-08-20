fbfcfe39ab46f3eda6c3c9357667b49988c77429
maywandnasri <maywandnasri@gmail.com>
2026-08-20 17:23:21 -0400

    Update refresh.js

--- pages/api/refresh.js
+++ pages/api/refresh.js
@@ -14,11 +14,13 @@ lastUpdated: new Date().toISOString(),
 
 async function getCalendarEvents() {
 const auth = new google.auth.JWT(
-process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-null,
-process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-const calendar = google.calendar({ version: "v3", auth });
-["https://www.googleapis.com/auth/" + "calendar.readonly"],const now = new Date();
+  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  ["https://www.googleapis.com/auth/" + "calendar.readonly"]
+);
+
+const calendar = google.calendar({ version: "v3", auth });["https://www.googleapis.com/auth/" + "calendar.readonly"],const now = new Date();
 const future = new Date();
 future.setDate(future.getDate() + 60);
 
